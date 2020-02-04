a8aefdafaf26be8aebdcaa27fe2e7ecc60aa0bf8
tilark <tilark.liu@gmail.com>
2020-02-04 23:11:34 +0800

    defaultinsetup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { SetupComponent } from './pages/setup/setup.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'setup', component: SetupComponent},
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'setup' },
 ];
 
 @NgModule({

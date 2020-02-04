16016172b2f41282a6e411ae4bfb87f4a06599c7
tilark <tilark.liu@gmail.com>
2020-02-04 23:10:36 +0800

    addsetup

--- src/app/pages/setup/setup.component.ts
+++ src/app/pages/setup/setup.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
+import {
+  USERNAME,
+  INIT_FLAG,
+  START_USING_DATE
+} from '../../services/local-storage/local-storage.service.namespace';
+import { getTodayTime } from 'src/app/utils/time';
 @Component({
   selector: 'app-setup',
   templateUrl: './setup.component.html',
   styleUrls: ['./setup.component.less']
 })
 export class SetupComponent implements OnInit {
-
-  constructor() { }
+  public username: string;
+  constructor(private store: LocalStorageService) { }
 
   ngOnInit() {
   }
 
+  completeSetup(): void {
+    this.store.set(INIT_FLAG, true);
+    this.store.set(START_USING_DATE, getTodayTime());
+    this.store.set(USERNAME, this.username);
+  }
 }
--- src/app/pages/setup/setup.module.ts
+++ src/app/pages/setup/setup.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SetupComponent } from './setup.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [SetupComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class SetupModule { }

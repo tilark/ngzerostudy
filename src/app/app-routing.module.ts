da31b6eed59cc37e73fd3d25634570d9f6b67a79
tilark <tilark.liu@gmail.com>
2020-02-04 22:16:04 +0800

    addngzorro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SetupComponent } from './pages/setup/setup.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'setup', component: SetupComponent},
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
